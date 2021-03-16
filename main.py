9bf40d5fc20609b8b3a4a869caaabea9564abcb8
radostmilena <radost.herboth@gmail.com>
2021-03-16 16:37:33 +0100

    Python 3 script to run dla_3d package.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
 import numpy as np
-import matplotlib.pyplot as plt
 from dla_3d import initialize_system, random_walk
 
+#initialize empty grid for k = 6, find and fill center
 isy = initialize_system(6)
 empty_grid = isy.initialize_grid()
 grid, center = isy.find_center()
 print(center)
 
+#initialize random walk for inner_radius = 28 and outer_radius = 30
 rw = random_walk(grid, center, 28, 30)
 
+#run random walk on defined grid
 for i in range(1, 35000, 1):
     grid = rw.walk(grid)
 
